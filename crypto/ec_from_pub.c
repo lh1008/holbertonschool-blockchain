b921445998d9171a45bcb341bd9226540874c17d
lh1008 <paintluis@protonmail.com>
2021-08-30 15:26:02 -0500

    Added files from task 3

---
+++ crypto/ec_from_pub.c
@@ -0,0 +1,12 @@
+#include "hblk_crypto.h"
+
+/**
+ * ec_from_pub - entry to function
+ * Desc: ec_from_pub function that creates
+ * EC_KEY structure given a public key
+ * @pub: const that contains the public key to be converted
+ * Return: pointer to the created EC_KEY, or NULL upon failure
+ */
+EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
+{
+}
