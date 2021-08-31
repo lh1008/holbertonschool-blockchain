06ad2ac02c978280b1caf3dfd3034007035ef74e
lh1008 <paintluis@protonmail.com>
2021-08-31 17:04:50 -0500

    Succesfully created EC key pair

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -7,8 +7,21 @@
  * @key: pointer that point to the EC_KEY structure
  * @msg: pointer that points to the msglen characters to be signed
  * @sig: pointer that holds the address at which to store the signature
+ * Return: pointer to the signature buffer upon success, NULL upon failure
  */
 uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
 		 size_t msglen, sig_t *sig)
 {
+	unsigned int len;
+
+	if (key == NULL || msg == NULL || sig == NULL)
+		return (NULL);
+
+	len = sig->len;
+	/*Sign the message*/
+	if (ECDSA_sign(0, msg, msglen, sig->sig, &len, (EC_KEY *)key) != 1)
+		return (NULL);
+	sig->len = len;
+
+	return (sig->sig);
 }
