52907e8d87320bb9266aa6a0eea08a2158d84210
lh1008 <paintluis@protonmail.com>
2021-08-31 16:21:12 -0500

    Task 5, ec_load EC key pair from the disk

---
+++ crypto/ec_load.c
@@ -0,0 +1,51 @@
+#include "hblk_crypto.h"
+
+/**
+ * ec_load - entry to function
+ * Desc: ec_load function that loads an
+ * EC key pair from the disk
+ * @folder: pointer string path folder from where to load keys
+ * Return: pointer to the created EC key pair on success
+ * NULL upon failure
+ */
+EC_KEY *ec_load(char const *folder)
+{
+	FILE *fp;
+	char path[256] = {0};
+	EC_KEY *key = NULL;
+
+	if (!folder)
+		return (0);
+
+	/*Create the complete path to read the public key*/
+	sprintf(path, "%s/" PUB_KEY, folder);
+	/*Open a file descriptor to read the public key*/
+	fp = fopen(path, "r");
+	if (!fp)
+	{
+		EC_KEY_free(key);
+		return (0);
+	}
+	/*Read the public key in the given file and store it in key*/
+	if (!PEM_read_EC_PUBKEY(fp, &key, NULL, NULL))
+	{
+		EC_KEY_free(key);
+		fclose(fp);
+		return (0);
+	}
+	fclose(fp);
+	/*Create the complete path to read the private key*/
+	sprintf(path, "%s/" PRIV_KEY, folder);
+	/*Open a file descriptor to read the private key*/
+	fp = fopen(path, "r");
+	if (!fp)
+		return (0);
+	/*read the private key in the given file and store it in key*/
+	if (!PEM_read_ECPrivateKey(fp, &key, NULL, NULL))
+	{
+		fclose(fp);
+		return (0);
+	}
+	fclose(fp);
+	return (key);
+}
