95e1acb788d5b6c4c0e18e9755ebe2b555319a9a
lh1008 <paintluis@protonmail.com>
2021-08-31 17:12:20 -0500

    Missing betty requirement

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -5,7 +5,8 @@
  * Desc: ec_sign function that signs a given set of bytes,
  * using a given EC_KEY private key
  * @key: pointer that point to the EC_KEY structure
- * @msg: pointer that points to the msglen characters to be signed
+ * @msg: pointer to msglen
+ * @msglen: size_t type characters to be signed
  * @sig: pointer that holds the address at which to store the signature
  * Return: pointer to the signature buffer upon success, NULL upon failure
  */
