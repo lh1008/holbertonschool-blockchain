5f444ae8f92760752ed2109325fd15395ad541d3
lh1008 <paintluis@protonmail.com>
2021-09-24 11:23:08 -0500

    Added prototype header for first task Create transaction output

--- blockchain/v0.3/blockchain.h
+++ blockchain/v0.3/blockchain.h
@@ -184,5 +184,6 @@ int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
 			    uint32_t difficulty);
 void block_mine(block_t *block);
 uint32_t blockchain_difficulty(blockchain_t const *blockchain);
+tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN]);
 
 #endif /* BLOCKCHAIN_H */
