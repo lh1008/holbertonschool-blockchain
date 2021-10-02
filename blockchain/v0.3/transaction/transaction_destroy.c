fc1aa3833fa161fbcffcd04d759f17bf29410caf
lh1008 <paintluis@protonmail.com>
2021-10-02 15:20:25 -0500

    Task 9, Delete transaction

---
+++ blockchain/v0.3/transaction/transaction_destroy.c
@@ -0,0 +1,20 @@
+#include "transaction.h"
+
+/**
+ * transaction_destroy - entry to function
+ * Desc: transaction_destroy that deallocates a transaction structure
+ * @transaction: points to the transaction to delete
+ * Return: Nothing
+ */
+void transaction_destroy(transaction_t *transaction)
+{
+	if (!transaction)
+	{
+		return;
+	}
+
+	llist_destroy(transaction->inputs, 1, free);
+	llist_destroy(transaction->outputs, 1, free);
+
+	free(transaction);
+}
