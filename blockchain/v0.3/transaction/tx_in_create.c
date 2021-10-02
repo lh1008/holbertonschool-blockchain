c67103e354d8e4ea91c79579d691f03027cc9247
lh1008 <paintluis@protonmail.com>
2021-10-02 14:29:42 -0500

    Task 2, create transaction input

--- blockchain/v0.3/transaction/tx_in_create.c
+++ blockchain/v0.3/transaction/tx_in_create.c
@@ -2,27 +2,33 @@
 
 /**
  * tx_in_create - entry to function
- * Desc: tx_in_create function that allocates and initializes a
- * transaction input structure
- * @unspent: points to the unpsent transaction output to be
+ * Desc: tx_in_create function that allocates and initializes
+ * a transaction input structure
+ * @unspent: points to the unspent transaction output to be
  * converted to a transaction input
- * Return: pointer to the created transaction input or NULL
+ * Return: a pointer to the created transaction input upon success,
+ * or NULL upon failure
  */
 tx_in_t *tx_in_create(unspent_tx_out_t const *unspent)
 {
-	tx_in_t *new_tx_in = NULL;
+	tx_in_t *new_tx_input = NULL;
 
 	if (unspent == NULL)
+	{
 		return (NULL);
+	}
+	new_tx_input =  calloc(1, sizeof(tx_in_t));
 
-	new_tx_in = calloc(1, sizeof(tx_in_t));
-
-	if (new_tx_in == NULL)
+	if (!new_tx_input)
+	{
 		return (NULL);
+	}
+	memcpy(new_tx_input->block_hash, unspent->block_hash,
+			sizeof(new_tx_input->block_hash));
+	memcpy(new_tx_input->tx_id, unspent->tx_id,
+			sizeof(new_tx_input->tx_id));
+	memcpy(new_tx_input->tx_out_hash, unspent->out.hash,
+			sizeof(new_tx_input->tx_out_hash));
 
-	memcpy(new_tx_in->block_hash, unspent->block_hash, sizeof(new_tx_in->block_hash));
-	memcpy(new_tx_in->tx_id, unspent->tx_id, sizeof(new_tx_in->tx_id));
-	memcpy(new_tx_in->tx_out_hash, unspent->out.hash, sizeof(new_tx_in->tx_out_hash));
-
-	return (new_tx_in);
+	return (new_tx_input);
 }
